4f19abf6b283b85c65e5a588b5f168710d855df1
Shravan <chaudharysharwan2000@gmail.com>
2024-03-23 15:29:08 +0530

    Added devise gem for login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_23_074547) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_23_082425) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -59,10 +59,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_23_074547) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
