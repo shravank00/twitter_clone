2d62752bf410f09c91d5a43de5a6a451394d67ed
Shravan <chaudharysharwan2000@gmail.com>
2024-03-24 20:44:56 +0530

    Added user details to tweets and added follow unfollow button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,2 @@
 module UsersHelper
-  def user_image(user)
-    user.avatar.attached? ? url_for(user.avatar) : 'default-user.jpg'
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
+  has_and_belongs_to_many :following, class_name: "User", join_table: "relationships", foreign_key: "follower_id", association_foreign_key: "followed_id"
+  has_and_belongs_to_many :followers, class_name: "User", join_table: "relationships", foreign_key: "followed_id", association_foreign_key: "follower_id"
+
   has_one_attached :avatar
   validates :username, uniqueness: true, if: :username_changed?
 end
